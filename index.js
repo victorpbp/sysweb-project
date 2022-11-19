0ad3cfbbd7453a216a1c473ba6867edd497aa7b1
victorpbp <victorrpeixotoo@gmail.com>
2022-11-19 19:42:57 -0300

    Feat: add remaining contestants

--- index.js
+++ index.js
@@ -4,40 +4,40 @@ const jogadores = [
     id: 0,
     number: 10,
     nome: "Neymar Jr",
-    pais: "Brazil",
+    pais: "Brasil",
     imagem: "https://www.futwiz.com/assets/img/fifa22/faces/184740247.png",
   },
 
   {
     id: 1,
-    number: "21",
-    nome: "Alguém 1",
-    pais: "Algum lugar",
-    imagem: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/271deea8-e28c-41a3-aaf5-2913f5f48be6/de7834s-6515bd40-8b2c-4dc6-a843-5ac1a95a8b55.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzI3MWRlZWE4LWUyOGMtNDFhMy1hYWY1LTI5MTNmNWY0OGJlNlwvZGU3ODM0cy02NTE1YmQ0MC04YjJjLTRkYzYtYTg0My01YWMxYTk1YThiNTUuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.BopkDn1ptIwbmcKHdAOlYHyAOOACXW0Zfgbs0-6BY-E",
+    number: 21,
+    nome: "Lionel Messi",
+    pais: "Argentina",
+    imagem: "https://www.futwiz.com/assets/img/fifa22/faces/167930183.png",
   },
 
   {
     id: 2,
-    number: "34",
-    nome: "Alguém 2",
-    pais: "Algum lugar",
-    imagem: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/271deea8-e28c-41a3-aaf5-2913f5f48be6/de7834s-6515bd40-8b2c-4dc6-a843-5ac1a95a8b55.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzI3MWRlZWE4LWUyOGMtNDFhMy1hYWY1LTI5MTNmNWY0OGJlNlwvZGU3ODM0cy02NTE1YmQ0MC04YjJjLTRkYzYtYTg0My01YWMxYTk1YThiNTUuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.BopkDn1ptIwbmcKHdAOlYHyAOOACXW0Zfgbs0-6BY-E",
+    number: 34,
+    nome: "Cristino Ronaldo",
+    pais: "Portugal",
+    imagem: "https://www.futwiz.com/assets/img/fifa22/faces/251679041.png",
   },
 
   {
     id: 3,
-    number: "57",
-    nome: "Alguém 3",
-    pais: "Algum lugar",
-    imagem: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/271deea8-e28c-41a3-aaf5-2913f5f48be6/de7834s-6515bd40-8b2c-4dc6-a843-5ac1a95a8b55.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzI3MWRlZWE4LWUyOGMtNDFhMy1hYWY1LTI5MTNmNWY0OGJlNlwvZGU3ODM0cy02NTE1YmQ0MC04YjJjLTRkYzYtYTg0My01YWMxYTk1YThiNTUuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.BopkDn1ptIwbmcKHdAOlYHyAOOACXW0Zfgbs0-6BY-E",
+    number: 57,
+    nome: "Mario Gotze",
+    pais: "Alemanha",
+    imagem: "https://www.futwiz.com/assets/img/fifa22/faces/50523966.png",
   },
 
   {
     id: 4,
-    number: "89",
-    nome: "Alguém 4",
-    pais: "Algum lugar",
-    imagem: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/271deea8-e28c-41a3-aaf5-2913f5f48be6/de7834s-6515bd40-8b2c-4dc6-a843-5ac1a95a8b55.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzI3MWRlZWE4LWUyOGMtNDFhMy1hYWY1LTI5MTNmNWY0OGJlNlwvZGU3ODM0cy02NTE1YmQ0MC04YjJjLTRkYzYtYTg0My01YWMxYTk1YThiNTUuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.BopkDn1ptIwbmcKHdAOlYHyAOOACXW0Zfgbs0-6BY-E",
+    number: 89,
+    nome: "Kylian Mbappe",
+    pais: "França",
+    imagem: "https://www.futwiz.com/assets/img/fifa22/faces/251889987.png",
   },
 
 ]
@@ -106,18 +106,16 @@ function procurarJogador(x , y) {
   let wantedNumber = x.concat(y);
 
   for (i=0 ; i < jogadores.length ; i++) {
-
     if (wantedNumber == jogadores[i].number) {
       nomeJogador.innerHTML = jogadores[i].nome;
       paisJogador.innerHTML = jogadores[i].pais;
       imagemJogador.src = jogadores[i].imagem;
       confirmAllow = 1;
+      feedback.innerHTML = "";
       return;
     }
-    
-    reset();
-    feedback.innerHTML = "Jogador não encontrado, por favor tente novamente";
-
   }
+  reset();
+  feedback.innerHTML = "Jogador não encontrado, por favor tente novamente";
 
 }
