7e2c8be6b665c1dadbf2a9b1d91f352a0aa5619e
victorpbp <victorrpeixotoo@gmail.com>
2022-11-19 18:16:23 -0300

    Init commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const jogadores = [
+  
+  {
+    id: 0,
+    number: 10,
+    nome: "Neymar Jr",
+    imagem: "./playerimages/neymar.jpg",
+  },
+
+  {
+    id: 1,
+    number: 21,
+    nome: "Alguém 1",
+    imagem: "./playerimages/neymar.jpg",
+  },
+
+  {
+    id: 2,
+    number: 34,
+    nome: "Alguém 2",
+    imagem: "./playerimages/neymar.jpg",
+  },
+
+  {
+    id: 3,
+    number: 57,
+    nome: "Alguém 3",
+    imagem: "./playerimages/neymar.jpg",
+  },
+
+  {
+    id: 4,
+    number: 89,
+    nome: "Alguém 4",
+    imagem: "./playerimages/neymar.jpg",
+  },
+
+]
